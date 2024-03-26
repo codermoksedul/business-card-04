847b1cf7b0cc7757c6160e2e558b44a597c60ec0
dmoksedul <dmoksedulislam@gmail.com>
2024-03-26 14:44:43 +0600

    add

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "primary-color" :"#FBB531"
+      },
+    },
+  },
+  plugins: [],
+}
